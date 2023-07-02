6fa2efc4924342e83802d2a66cf68738f0851da2
MOHDABDULKHADAR11 <arbazmohammed11@gmail.com>
2023-07-02 19:21:42 +0530

    full-stack-app-final-code

--- models/http-error.js
+++
@@ -1,8 +0,0 @@
-class HttpError extends Error { // we are making use of Error predefined class to make our own error handling model
-    constructor(message,errorCode){
-        super(message);
-        this.code = errorCode
-    }
-}
-
-module.exports = HttpError;
